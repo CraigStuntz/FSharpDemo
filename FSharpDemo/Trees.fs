634abd17ef155adea0e3721c2a886328d38ac431
CraigStuntz <craig_stuntz@acm.org>
2014-05-12 23:19:24 -0400

    Initial commit

---
+++ FSharpDemo/Trees.fs
@@ -0,0 +1,7 @@
+﻿module Trees
+
+    type 'T tree = 
+        | Tree of 'T * 'T tree * 'T tree
+        | Leaf of 'T
+
+    let testTree = Tree("1", Tree("2", Leaf "a", Leaf "b"), Leaf "c")
---
+++ FSharpDemo/UnitsOfMeasure.fs
@@ -0,0 +1,5 @@
+﻿module UnitsOfMeasure
+    open Microsoft.FSharp.Data.UnitSystems.SI.UnitNames
+    open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+
+    let momentum (mass: float<kg>) (velocity : float<m/s^2>) = mass * velocity
